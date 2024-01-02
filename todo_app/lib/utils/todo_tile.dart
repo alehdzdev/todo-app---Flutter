e898c7381454d39c6d91bc034a0ea2cead4f366f
Alejandro Hernandez <alehdzdev@gmail.com>
2024-01-02 09:20:04 -0400

    [ADD] child for text and checkbox

--- todo_app/lib/utils/todo_tile.dart
+++ todo_app/lib/utils/todo_tile.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+
+  const ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25.0),
       child: Container(
-        padding: EdgeInsets.all(24),
-        child: Text("Make tutorial"),
+        padding: const EdgeInsets.all(24),
         decoration: BoxDecoration(
           color: Colors.blueGrey,
           borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            const Text("Make tutorial"),
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+          ],
         ),
       ),
     );
