0a3a81ac827bb49736c98b16284a19d11aca7260
Alejandro Hernandez <alehdzdev@gmail.com>
2024-01-04 09:33:47 -0400

    [ADD] checkbox and linethrough text

--- todo_app/lib/utils/todo_tile.dart
+++ todo_app/lib/utils/todo_tile.dart
@@ -15,7 +15,7 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
       child: Container(
         padding: const EdgeInsets.all(24),
         decoration: BoxDecoration(
@@ -24,8 +24,19 @@ class ToDoTile extends StatelessWidget {
         ),
         child: Row(
           children: [
-            const Text("Make tutorial"),
-            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: taskCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
           ],
         ),
       ),
